9ebba68c59292db645d307609148bac47da3a09f
christina parece <christinaparece@gmail.com>
2021-10-07 16:32:58 -0400

    day3HW

---
+++ my_mod.py
@@ -0,0 +1,11 @@
+def area_of_house(length,width):
+    result= length * width
+    print(result)
+    
+  
+    
+         
+def my_c_function(radius):
+    import math
+    c= 2* math.pi *radius
+    print(c)
